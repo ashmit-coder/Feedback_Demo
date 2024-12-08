0af6a8a54e940d4a8d94f9c6d34ff741a24bb853
Anshul Kumar <anshulk0102@gmail.com>
2024-12-08 06:19:53 +0530

    changed feedback route

--- app/api/feedback/update/route.ts
+++ app/api/feedback/update/route.ts
@@ -2,7 +2,7 @@ import { NextRequest,NextResponse } from "next/server";
 import {feedback,UpdateFeedback }from "../../models/user";
 import mongoose from "mongoose";
 
-export  async function POST(req: NextRequest, res: NextResponse) {
+export  async function POST(req: NextRequest) {
     try {
         let db = await mongoose.connect((process.env.MONGO_URI as string))
         let data =  await req.json();
