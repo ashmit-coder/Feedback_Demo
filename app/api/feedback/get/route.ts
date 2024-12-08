575487e909f88f3bc0cab493425c0689609df0db
Anshul Kumar <anshulk0102@gmail.com>
2024-12-08 06:15:32 +0530

    route.ts changed

--- app/api/feedback/get/route.ts
+++ app/api/feedback/get/route.ts
@@ -2,11 +2,11 @@ import { NextRequest, NextResponse } from "next/server";
 import { feedback } from "../../models/user";
 import mongoose from "mongoose";
 
-export async function GET(req: NextRequest, res: NextResponse) {
+export async function GET(req: NextRequest) {
   try {
     await mongoose.connect(process.env.MONGO_URI as string);
 
-    // Fetch the latest 3 feedbacks based on feedbackID (descending order)
+    
     let data = await feedback.find().sort({ feedbackID: -1 }).limit(3);
 
     return NextResponse.json({
